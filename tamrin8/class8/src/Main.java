0de9b6099a4baeb5320a42beffe2f460f0256ea3
mohammad amin ghorbani <63232995+magh20@users.noreply.github.com>
2020-06-03 13:18:26 +0430

    tamrin8

---
+++ tamrin8/class8/src/Main.java
@@ -0,0 +1,13 @@
+import controller.PersonContr;
+import view.Form;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        PersonContr personContr=new PersonContr();
+        Form form=new Form();
+    }
+}
