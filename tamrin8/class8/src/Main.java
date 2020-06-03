2426491eac61d9dbc709ee9022476b363b4ebc23
mohammad amin ghorbani <63232995+magh20@users.noreply.github.com>
2020-06-03 13:20:52 +0430

    tamrin8

--- tamrin8/class8/src/Main.java
+++ tamrin8/class8/src/Main.java
@@ -6,8 +6,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        PersonContr personContr=new PersonContr();
         Form form=new Form();
     }
 }
